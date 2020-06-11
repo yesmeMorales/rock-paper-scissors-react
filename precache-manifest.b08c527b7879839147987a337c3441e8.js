34bd3a0bd093022ee84262e0cbbeec4423bdf31e
Yesme <moralesyesme@gmail.com>
2020-06-11 14:50:08 -0500

    Updates

---
+++ precache-manifest.b08c527b7879839147987a337c3441e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1f506712c67956413af853a7c0ae3d2",
+    "url": "/rock-paper-scissors-react/index.html"
+  },
+  {
+    "revision": "d4a8d6dec0693e967ab7",
+    "url": "/rock-paper-scissors-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1d2a8f1348f2e7a709ce",
+    "url": "/rock-paper-scissors-react/static/js/2.8a2def42.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rock-paper-scissors-react/static/js/2.8a2def42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4a8d6dec0693e967ab7",
+    "url": "/rock-paper-scissors-react/static/js/main.eff959c7.chunk.js"
+  },
+  {
+    "revision": "4901db6cbc35c496823a",
+    "url": "/rock-paper-scissors-react/static/js/runtime-main.bd7da733.js"
+  }
+]);
